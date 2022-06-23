db85b23834646654ef567b91e96f84ffcdbc7fcb
Kacper Hes <kacperhes@kacpers-macbook-air.home>
2022-06-23 18:37:24 +0200

    chore(): Clenup router, styles and views * create views folder * create routes folder * move styles to assets folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+
+import { HOME } from "./routes";
+import { Home } from "./views";
+
+/**
+ * main component containing router
+ */
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={HOME} element={<Home />} />
+    </Routes>
   );
 }
 
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,13 @@
+/**
+ * main view
+ * @route /
+ */
+const Home = () => {
+    return (
+        <div>
+            <p>Hello World!</p>
+        </div>
+    )
+}
+
+export default Home;
