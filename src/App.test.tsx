c972312559b44770ae78fb4ed733c2b58cc2c125
Kacper Hes <kacperhes@kacpers-macbook-air.home>
2022-06-23 18:53:54 +0200

    fix(): Fix app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,12 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-test("renders learn react link", () => {
+test("renders Hello, World!", () => {
   render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
   );
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello, World!/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello, World!
       </h1>
     </div>
   );
