b96fd7c0fb7425141db3cb5193a5ba9ad3e64ad9
Kacper Hes <kacperhes@kacpers-macbook-air.home>
2022-06-23 18:52:09 +0200

    config(tailwind): Config tailwindcss for project

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 /**
  * main view
  * @route /
@@ -5,7 +7,9 @@
 const Home = () => {
   return (
     <div>
-      <p>Hello World!</p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 };
